8a35f032f6c619538c9202e81bf843de5e3dcf36
Troels Knak-Nielsen <troelskn@gmail.com>
2013-05-21 21:35:58 +0200

    Backported old v1 calls and marked them as deprecated

--- lib/shopsense/api.rb
+++ lib/shopsense/api.rb
@@ -1,16 +1,14 @@
 module Shopsense
 
-  API_ENDPOINT = 'http://api.shopstyle.com/api/v2'
-  FILTER_TYPES = ['Brands', 'Retailer', 'Price', 'Discount', 'Size', 'Color'].freeze
+  API_ENDPOINTS = {1 => 'http://api.shopstyle.com/action', 2 => 'http://api.shopstyle.com/api/v2'}
+  FILTER_TYPES = ['Category', 'Brand', 'Retailer', 'Price', 'Discount', 'Size', 'Color'].freeze
   LOOK_TYPES = ['New', 'TopRated', 'Celebrities', 'Featured'].freeze
 
   class API
 
-    attr_reader :configuration
-
     def initialize(args = {})
       @partner_id = args['partner_id']
-      @unserialize = args['unserialize']
+      @unserialize = args['unserialize'].nil? ? true : args['unserialize']
       @site = args['site'] || 'www.shopstyle.com'
     end
 
@@ -24,15 +22,13 @@ module Shopsense
     # @return  A list of Product objects. Each Product has an id, name,
     #   description, price, retailer, brand name, categories, images in small/medium/large,
     #   and a URL that forwards to the retailer's site.
-    def search(search_string, opts = {})
-      opts = {:offset => 0, :limit => 10}.merge(opts)
-      raise "no search string provided!" if search_string.nil?
-      args = {
-        :fts => search_string,
-        :offset => opts[:offset],
-        :limit => opts[:limit]
-      }
-      call_api('/products', args)
+    def products(opts = {})
+      raise "Interface changed. Use as :query => 'search'" if opts.kind_of? String
+      call_api('/products', build_product_query(opts))
+    end
+
+    def product_by_id(product_id)
+      call_api('/products/' + CGI::escape(product_id.to_s))
     end
 
     # This method returns a list of categories and product counts that describe the results
@@ -40,13 +36,8 @@ module Shopsense
     # @param  [String]  search_string The string to be in the query.
     # @return [String]  A list of Category objects. Each Category has an id, name, and count
     #   of the number of query results in that category.
-    def category_histogram(search_string)
-      raise "no search string provided!" if search_string.nil?
-      args = {
-        :fts => search_string,
-        :filters => 'Category'
-      }
-      call_api('/histogram', args)
+    def category_histogram(opts)
+      filter_histogram('Category', opts)
     end
 
     # This method returns a list of categories and product counts that describe the results
@@ -55,36 +46,37 @@ module Shopsense
     #   The type of filter data to return. Possible values are
     #   Brand, Retailer, Price, Discount, Size and Color.
     # @param  [String]  search_string The string to be in the query.
-    # @return [String]  A list of Category objects. Each Category has an id, name, and count
-    #   of the number of query results in that category.
-    def filter_histogram(filter_type, search_string)
-      raise "invalid filter type" unless FILTER_TYPES.include?(filter_type)
-      raise "no search string provided!" if search_string.nil?
-      args = {
-        :fts => search_string,
-        :filters => filter_type
-      }
-      call_api('/histogram', args)
+    # @return [String]  A list of Filter objects of the given type. Each Filter has an id,
+    #                   name, and count of the number of results that apply to that filter.
+    def filter_histogram(filter_type, opts = {})
+      raise "Interface changed. Use as :query => 'search'" if opts.kind_of? String
+      filter_types = [*filter_type]
+      filter_types.each do |f|
+        raise "invalid filter type" unless FILTER_TYPES.include?(f)
+      end
+      args = build_product_query(opts)
+      args[:filters] = filter_types.join(",")
+      args[:floor] = opts[:floor]
+      call_api('/products/histogram', args)
+    end
+
+    # This method returns a list of the categories available to the API.
+    # @return A list of all categories, with id, name, and parent id.
+    def categories
+      call_api('/categories')
+    end
+
+    # This method returns the list of canonical colors available.
+    # @return A list of all Colors, with id, name, and url of each.
+    def colors
+      call_api('/colors')
     end
 
     # This method returns a list of brands that have live products. Brands that have
     # very few products will be omitted.
-    # @return [String] A list of all Brands, with id, name, url, and synonyms of each.
+    # @return A list of all Brands, with id, name, url, and synonyms of each.
     def brands
       call_api('/brands')
-    end
-
-    # This method returns information about a particular look and its products.
-    # @param [Integer] look_id
-    #   The ID number of the look. An easy way to get a look's ID is
-    #   to go to the Stylebook page that contains the look at the ShopStyle website and
-    #   right-click on the button that you use to edit the look. From the popup menu, select
-    #   "Copy Link" and paste that into any text editor. The "lookId" query parameter of that
-    #   URL is the value to use for this API method.
-    # @return [String]  single look, with title, description, a set of tags, and a list of products.
-    #   The products have the fields listed (see #search)
-    def look(look_id)
-      raise "TODO"
     end
 
     # This method returns a list of retailers that have live products.
@@ -104,10 +96,31 @@ module Shopsense
     # @return [String]
     #   A look id of the user's Stylebook, the look id of each individual look within that Stylebook,
     #   and the title and description associated with each look.
-    def stylebook(user_name, opts = {})
-      raise "TODO"
+    def stylebook(opts = {})
+      opts = {:offset => 0, :limit => 10}.merge(opts)
+      args = {
+        :handle => opts[:username],
+        :min => opts[:offset],
+        :count => opts[:limit],
+      }
+      call_api('/apiGetStylebook', args, 1)
     end
 
+    # @deprecated
+    # This method returns information about a particular look and its products.
+    # @param [Integer] look_id
+    #   The ID number of the look. An easy way to get a look's ID is
+    #   to go to the Stylebook page that contains the look at the ShopStyle website and
+    #   right-click on the button that you use to edit the look. From the popup menu, select
+    #   "Copy Link" and paste that into any text editor. The "lookId" query parameter of that
+    #   URL is the value to use for this API method.
+    # @return [String]  single look, with title, description, a set of tags, and a list of products.
+    #   The products have the fields listed (see #search)
+    def look(look_id)
+      call_api('/apiGetLook', {:look => look_id}, 1)
+    end
+
+    # @deprecated
     # This method returns information about looks that match different kinds of searches.
     # @param [Integer] look_type
     #   The type of search to perform. Supported values are:
@@ -115,8 +128,6 @@ module Shopsense
     #   TopRated - Recently created looks that are highly rated.
     #   Celebrities - Looks owned by celebrity users.
     #   Featured - Looks from featured stylebooks.
-    # @param [String] username
-    #   The username of the Stylebook owner.
     # @param  [Integer] index
     #   The start index of results returned.
     # @param  [Integer] num_results
@@ -124,10 +135,19 @@ module Shopsense
     # @return [String]
     #   A list of looks of the given type.
     def looks(look_type, opts = {})
-      raise "TODO"
+      opts = {:offset => 0, :limit => 10}.merge(opts)
+      args = {
+        :type => look_type,
+        :min => opts[:offset],
+        :count => opts[:limit],
+      }
+      call_api('/apiGetLooks', args, 1)
     end
 
-    # TODO:
+    # @deprecated
+    # This method provides a link that will redirect to the retailer product page.
+    # You probably won't need this as the link is also included with the product structs.
+    #
     # This method does not return a reponse of XML or JSON data like the other elements of the API.
     # Instead, it forwards the user to the retailer's product page for a given product. It is the
     # typical behavior to offer when the user clicks on a product. The apiSearch method returns URLs
@@ -139,10 +159,11 @@ module Shopsense
     #   editor. The "id" query parameter of that URL is the value to use for this API method.
     # @return [String]
     #   A web link to the retailer. $1 of the $0
-    def visit_retailer(id)
-      raise "TODO"
+    def visit_retailer_href(product_id)
+      API_ENDPOINTS[1] + "/apiVisitRetailer?pid=#{CGI::escape(@partner_id.to_s)}&id=#{CGI::escape(product_id.to_s)}"
     end
 
+    # @deprecated
     # This method returns the popular brands for a given category along with a sample product for the
     # brand-category combination.
     # @param [String] category
@@ -150,25 +171,52 @@ module Shopsense
     #   parameter. If category is not supplied, all the popular brands regardless of category will be returned.
     # @return [String] A list of trends in the given category. Each trend has a brand, category, url, and
     #   optionally the top-ranked product for each brand/category.
-    def trends(category = "", products = 0)
+    def trends(opts = {})
+      opts = {:include_products => true}.merge(opts)
       args = {
-        :cat => category,
-        :products => products
+        :cat => opts[:category],
+        :products => opts[:include_products] ? nil : "0"
       }
-      call_api('/trends', args)
+      call_api('/apiGetTrends', args, 1)
     end
 
-    # Deprecated - Kept here for BC
-    alias_method :get_category_histogram, :category_histogram
-    alias_method :get_filter_histogram, :filter_histogram
-    alias_method :get_brands, :brands
-    alias_method :get_look, :look
-    alias_method :get_retailers, :retailers
-    alias_method :get_stylebook, :stylebook
-    alias_method :get_looks, :looks
-    alias_method :get_trends, :trends
-
     private
+
+      def build_product_query(opts = {})
+        opts = {:offset => 0, :limit => 10}.merge(opts)
+        args = {
+          :fts => opts[:query],
+          :offset => opts[:offset],
+          :limit => opts[:limit],
+          :cat => opts[:category_id]
+        }
+        filters = []
+        {:brand_id => "b", :retailer_id => "r", :price => "p", :sale => "d", :size => "s", :color => "c"}.each do |key,prefix|
+          if opts[key]
+            [*opts[key]].each do |value|
+              filters << "#{prefix}#{value}"
+            end
+          end
+        end
+        if filters.any?
+          args[:fl] = filters
+        end
+        if opts[:price_dropped_since]
+          d = opts[:price_dropped_since]
+          args[:pdd] = d.kind_of?(DateTime) ? d.to_time.to_i : d.to_i
+        end
+        args[:sort] = case opts[:sort_by]
+                      when :price_lo_hi
+                        'PriceLoHi'
+                      when :price_hi_lo
+                        'PriceHiLo'
+                      when :recency
+                        'Recency'
+                      when :popular
+                        'Popular'
+                      end
+        args
+      end
 
       # This method is used for making the http calls building off the DSL of this module.
       # @param [String] method
@@ -176,20 +224,24 @@ module Shopsense
       # @param [String] args
       #   A concatenated group of arguments seperated by a an & symbol and spces substitued with a + symbol.
       # @return [String] A list of the data returned
-      def call_api(relative_url, args = {})
-        base_url = API_ENDPOINT + relative_url
+      def call_api(relative_url, args = {}, api_version = 2)
         args[:pid] = @partner_id
         args[:format] = 'json'
         args[:site] = @site
-        if base_url.include?("?")
-          base_url.chomp!("&")
-          base_url << "&"
-        else
-          base_url << "?"
+        compiled_args = args.delete_if {|k,v| v.nil? }.map {|(k,v)|
+          if v.kind_of? Array
+            v.map {|vv| "#{CGI::escape(k.to_s)}=#{CGI::escape(v.to_s)}" }.join("&")
+          else
+            "#{CGI::escape(k.to_s)}=#{CGI::escape(v.to_s)}"
+          end
+        }.join("&")
+        href = API_ENDPOINTS[api_version] + relative_url + "?" + compiled_args
+        uri = URI.parse(href)
+        response = Net::HTTP.get_response(uri)
+        data = response.body
+        if response.code.to_i > 299 || response.code.to_i < 200
+          raise data
         end
-        base_url << args.map {|(k,v)| "#{CGI::escape(k.to_s)}=#{CGI::escape(v.to_s)}" }.join("&")
-        uri = URI.parse(base_url)
-        data = Net::HTTP.get(uri)
         if @unserialize
           require 'multi_json'
           MultiJson.load(data, :symbolize_keys => true)
--- spec/shopsense/shopsense_spec.rb
+++ spec/shopsense/shopsense_spec.rb
@@ -14,20 +14,20 @@ describe Shopsense do
         fts = 'something'
         min = 10
         count = 20
-        api.search(fts, :offset => min, :limit => count).should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/api/v2/products?pid=#{partner_id}&format=json&site=us&fts=#{fts}&offset=#{min}&limit=#{count}"))
+        api.products(:query => fts, :offset => min, :limit => count).should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/api/v2/products?pid=#{partner_id}&format=json&site=us&fts=#{fts}&offset=#{min}&limit=#{count}"))
       end
     end
     describe "get_category_histogram" do
       it "it passes if the proper data is returned" do
         fts = 'something'
-        api.category_histogram(fts).should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/api/v2/histogram?pid=#{partner_id}&format=json&site=us&filters=Category&fts=#{fts}"))
+        api.category_histogram(:query => fts).should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/api/v2/products/histogram?pid=#{partner_id}&format=json&site=us&filters=Category&fts=#{fts}"))
       end
     end
     describe "get_filter_histogram" do
       it "it passes if the proper data is returned" do
-        filter_type = 'Brands'
+        filter_type = 'Brand'
         fts = 'something'
-        api.filter_histogram(filter_type, fts).should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/api/v2/histogram?pid=#{partner_id}&format=json&site=us&filterType=#{filter_type}&fts=#{fts}"))
+        api.filter_histogram(filter_type, :query => fts).should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/api/v2/products/histogram?pid=#{partner_id}&format=json&site=us&filters=#{filter_type}&fts=#{fts}"))
       end
     end
     describe "get_brands" do
@@ -35,40 +35,36 @@ describe Shopsense do
         api.brands.should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/api/v2/brands?pid=#{partner_id}&format=json&site=us"))
       end
     end
-    # describe "get_look" do
-    #   it "it passes if the proper data is returned" do
-    #     look_id = 548347
-    #     api.look( look_id).should == Net::HTTP.get(URI.parse("#{configuration.api_url}#{configuration.get_look_path}?pid=#{configuration.partner_id}&format=#{configuration.format}&site=#{configuration.site}&look=#{look_id}"))
-    #   end
-    # end
+    describe "get_look" do
+      it "it passes if the proper data is returned" do
+        look_id = 548347
+        api.look(look_id).should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/action/apiGetLook?look=#{look_id}&pid=#{partner_id}&format=json&site=us"))
+      end
+    end
     describe "get_retailers" do
       it "it passes if the proper data is returned" do
         api.retailers.should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/api/v2/retailers?pid=#{partner_id}&format=json&site=us"))
       end
     end
-    # describe "get_stylebook" do
-    #   it "it passes if the proper data is returned" do
-    #     handle = 'KalvinTestone'
-    #     min = 0
-    #     count = 10
-    #     api.stylebook(handle, :offset => min, :limit => count).should == Net::HTTP.get(URI.parse("#{configuration.api_url}#{configuration.get_stylebook_path}?pid=#{configuration.partner_id}&format=#{configuration.format}&site=#{configuration.site}&handle=#{handle}&min=#{min}&count=#{count}"))
-    #   end
-    # end
-=begin
+    describe "get_stylebook" do
+      it "it passes if the proper data is returned" do
+        handle = 'KalvinTestone'
+        min = 0
+        count = 10
+        api.stylebook(:username => handle, :offset => min, :limit => count).should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/action/apiGetStylebook?pid=#{partner_id}&format=json&site=us&handle=#{handle}&min=#{min}&count=#{count}&handle=KalvinTestone"))
+      end
+    end
     describe "get_looks" do
       it "it passes if the proper data is returned" do
         look_type = 'New'
         min = 0
         count = 10
-        api.looks(look_type, :offset => min, :limit => count).should == Net::HTTP.get(URI.parse("#{configuration.api_url}#{configuration.get_looks_path}?pid=#{configuration.partner_id}&format=#{configuration.format}&site=#{configuration.site}&type=#{look_type}&min=#{min}&count=#{count}"))
+        api.looks(look_type, :offset => min, :limit => count).should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/action/apiGetLooks?type=#{look_type}&min=#{min}&count=#{count}&pid=#{partner_id}&format=json&site=us"))
       end
     end
-=end
     describe "get_trends" do
       it "it passes if the proper data is returned" do
-        category = 0
-        products = 0
-        api.trends(category, products).should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/api/v2/trends?pid=#{partner_id}&format=json&site=us&cat=#{category}&products=#{products}"))
+        api.trends.should == Net::HTTP.get(URI.parse("http://api.shopstyle.com/action/apiGetTrends?pid=#{partner_id}&format=json&site=us"))
       end
     end
 
@@ -79,7 +75,7 @@ describe Shopsense do
           fts = 'something'
           min = 10
           count = 20
-          api.search(fts, :offset => min, :limit => count).should have_key(:metadata)
+          api.search(:query => fts, :offset => min, :limit => count).should have_key(:metadata)
         end
       end
     end
